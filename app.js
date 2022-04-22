56e3a79ecfdd605d70fdf2904ac86a19438719f9
Emily  Phee <emilyphee@dhcp-v006-247.mobile.uci.edu>
2022-04-21 18:33:00 -0700

    Update play method with buttons selection

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const selectionBtns = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+
 let playerWinCount = 0;
 let computerWinCount = 0;
 
 function computerPlay() {
-  const choices = ['Rock', 'Scissor', 'Paper'];
+  const choices = ['Rock', 'Scissors', 'Paper'];
 
   // generate random number between index 0 to 2
   let randomInx = Math.floor(Math.random() * 3);
 
+  computer.innerText = choices[randomInx].toUpperCase();
   return choices[randomInx];
 }
 
@@ -18,25 +27,25 @@ function playRound(playerSelection, computerSelection) {
   if (player === 'rock') {
     if (computer === 'rock') {
       return "It's tie! Rock ties with rock";
-    } else if (computer === 'scissor') {
+    } else if (computer === 'scissors') {
       playerWinCount++;
-      return 'You won! Rock beats scissor';
+      return 'You won! Rock beats scissors';
     } else if (computer === 'paper') {
       computerWinCount++;
       return 'You Lose! Rock beaten by paper';
     }
   }
 
-  // when user select scissor
-  if (player === 'scissor') {
-    if (computer === 'scissor') {
-      return "It's tie! Scissor ties with scissor";
+  // when user select scissors
+  if (player === 'scissors') {
+    if (computer === 'scissors') {
+      return "It's tie! scissors ties with scissors";
     } else if (computer === 'paper') {
       playerWinCount++;
-      return 'You won! Scissor beats paper';
+      return 'You won! scissors beats paper';
     } else if (computer === 'rock') {
       computerWinCount++;
-      return 'You Lose! Scissor beaten by rock';
+      return 'You Lose! scissors beaten by rock';
     }
   }
 
@@ -47,24 +56,38 @@ function playRound(playerSelection, computerSelection) {
     } else if (computer === 'rock') {
       playerWinCount++;
       return 'You won! Paper beats rock';
-    } else if (computer === 'scissor') {
+    } else if (computer === 'scissors') {
       computerWinCount++;
-      return 'You Lose! Paper beaten by scissor';
+      return 'You Lose! Paper beaten by scissors';
     }
   }
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt(
-      'Type one of the options: Rock | Papaer | Scissor'
-    );
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+  for (let btn of selectionBtns) {
+    btn.addEventListener('click', () => {
+      const computerSelection = computerPlay();
+      let selectBtnVal = btn.id;
+      player.innerText = btn.id.toUpperCase();
+      result.innerText = playRound(selectBtnVal, computerSelection);
+
+      // Display current score each side
+      playerScore.innerText = playerWinCount;
+      computerScore.innerText = computerWinCount;
+
+      if (playerWinCount >= 5 || computerWinCount >= 5) {
+        winner();
+      }
+    });
   }
 
+  // let playerSelection = prompt(
+  //   'Type one of the options: Rock | Papaer | scissors'
+  // );
+  // const computerSelection = computerPlay();
+  // console.log(playRound(playerSelection, computerSelection));
   //   after 5 game play, ends game and display the result
-  winner();
+  // winner();
 }
 
 function winner() {
